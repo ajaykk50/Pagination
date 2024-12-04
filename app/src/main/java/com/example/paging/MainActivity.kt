790aa3290b02a2e955c604ac4ce2b1355d3cd4d8
akuzhiyil <akuzhiyil@abana.com.sa>
2024-12-04 11:19:32 +0530

    Background Color Changed to red

--- app/src/main/java/com/example/paging/MainActivity.kt
+++ app/src/main/java/com/example/paging/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.paging.ui.theme.PagingTheme
 
@@ -24,9 +27,20 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    Background()
+
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Background(){
+    Column(
+        modifier = Modifier.background(color = Color.Red)
+    ) {
+
     }
 }
 
